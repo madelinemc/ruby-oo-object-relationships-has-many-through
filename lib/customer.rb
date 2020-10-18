51c9fb68914c2eeebfb5b4fe3c93ed905e3b715d
madelinemc <madeline.coons@gmail.com>
2020-10-18 13:02:11 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,35 @@
 class Customer
-  
+
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip=0) #this method creates new meals as a customer and automatically associates each new meal with the customer that created it. 
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals #customer looks at all of the meals and selects the ones that belong to them
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    def waiters #uses meals method which selects all the meals that belong to the customer instane and then further iterates over just those meals to return who the waiter is.
+        meals.map do |meal|
+            meal.waiter
+        end
+    end
+
+
+    
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,34 @@
 class Waiter
 
+    attr_accessor :name, :yrs_experience
+
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0) #allows instance of Waiter class to create a new instance of Meal. set themselves as the waiter. This is the same as the customer new_meal method as the arugemnts remain the same, just the parameters are different(the self is in the corresponding spot for each class)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals #iterates over the meal class and selcts the meal instances that correspond to this waiter instance
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def best_tipper #uses meals method and allows waiter instance to access info from meal class and therefore from customer class thru meal class. 
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+            meal_a.tip <=> meal_b.tip
+        end
+        best_tipped_meal.customer
+    end
+
 end
